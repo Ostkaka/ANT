55e401931163b99e790a17ca3e69c79d614410ec
Ostkaka <tomasberglund005@gmail.com>
2013-05-29 22:39:36 +0200

    fdshfjd

--- cmake/antMacros.cmake
+++ cmake/antMacros.cmake
@@ -1,6 +1,8 @@
 #
 # Link libraires macro
 #
+# TODO - add handkle for sfml debug case. Also add the the case for the .lib endings and debug prefix
+#
 MACRO(ANT_LINK_LIBRARIES TARGET)
 	FOREACH(libname ${ARGN})
 		TARGET_LINK_LIBRARIES(${TARGET} optimized "${libname}" debug "${libname}${CMAKE_DEBUG_POSTFIX}")
